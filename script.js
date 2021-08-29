757ea1216b9e595a16e7f620432fdf659227fd28
JonasRossen <jona7906@stud.kea.dk>
2021-08-30 00:10:55 +0200

    adding inputs/sum of inputs

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ let first_input;
 let second_input;
 
 document.querySelector("#firstnumber").addEventListener("input", firstNumber);
+
 function firstNumber(){
-    first_input = this.value;
+    first_input = parseInt(this.value);
     console.log("first input = " + first_input);
 }
 
 document.querySelector("#secondnumber").addEventListener("input", secondNumber);
+
 function secondNumber(){   
-    second_input = this.value;
+    second_input =  parseInt(this.value); 
     console.log("second input = " + second_input);
 }
+
+document.querySelector("#calculate").addEventListener("click", calculateNumbers);
+
+function calculateNumbers(){
+    let sum = first_input + second_input;
+    console.log(sum);
+}
+
