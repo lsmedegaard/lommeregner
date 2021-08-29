cf57e0249029ec2086a3f7ca3dc6894eb76bdf12
JonasRossen <jona7906@stud.kea.dk>
2021-08-29 23:37:51 +0200

    changing variables with inputs

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+let first_input;
+let second_input;
+
 document.querySelector("#firstnumber").addEventListener("input", firstNumber);
 function firstNumber(){
-    console.log(this.value);
+    first_input = this.value;
+    console.log("first input = " + first_input);
+}
+
+document.querySelector("#secondnumber").addEventListener("input", secondNumber);
+function secondNumber(){   
+    second_input = this.value;
+    console.log("second input = " + second_input);
 }
